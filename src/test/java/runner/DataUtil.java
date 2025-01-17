b334c0dd3676c65ae0f09258f5e8980193cc6f2d
Indra Prajapati <>
2025-01-17 13:50:14 +0530

    Implemented Logic To Execute Suites Programatically Using Json.

---
+++ src/test/java/runner/DataUtil.java
@@ -0,0 +1,35 @@
+package runner;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class DataUtil {
+
+	public Map<String, String> loadClassMethods() {
+		String filePath = System.getProperty("user.dir") + "/src/test/resources/projectJSONs/classmethods.json";
+		Map<String, String> classMethodMap = new HashMap<String, String>();
+		JSONParser parser = new JSONParser();
+		try {
+			JSONObject classJSON = (JSONObject) parser.parse(new FileReader(new File(filePath)));
+			JSONArray classDetails = (JSONArray) classJSON.get("classdetails");
+			for (int i = 0; i < classDetails.size(); i++) {
+				JSONObject classDetail = (JSONObject) classDetails.get(i);
+				String className = (String) classDetail.get("class");
+				JSONArray methods = (JSONArray) classDetail.get("methods");
+				for (int j = 0; j < methods.size(); j++) {
+					String methodName = (String) methods.get(j);
+					classMethodMap.put(methodName, className);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return classMethodMap;
+	}
+}
---
+++ src/test/java/runner/JSONRunner.java
@@ -0,0 +1,81 @@
+package runner;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class JSONRunner {
+
+	public static void main(String[] args) {
+		Map<String, String> classMethodMap = new DataUtil().loadClassMethods();
+		String filePath = System.getProperty("user.dir") + "/src/test/resources/projectJSONs/testconfig.json";
+		JSONParser parser = new JSONParser();
+		try {
+			JSONObject classJSON = (JSONObject) parser.parse(new FileReader(new File(filePath)));
+			String parallelsuites = (String) classJSON.get("parallelsuites");
+			JSONArray testsuites = (JSONArray) classJSON.get("testsuites");
+			TestNGRunner testNG = new TestNGRunner(Integer.parseInt(parallelsuites));
+			for (int i = 0; i < testsuites.size(); i++) {
+				JSONObject testSuite = (JSONObject) testsuites.get(i);
+				String runMode = (String) testSuite.get("runmode");
+				if (runMode.equalsIgnoreCase("Yes")) {
+					String suiteName = (String) testSuite.get("name");
+					String paralleltests = (String) testSuite.get("paralleltests");
+					String suiteFileName = (String) testSuite.get("suitefilename");
+					boolean pTest = false;
+					if (paralleltests.equalsIgnoreCase("Yes")) {
+						pTest = true;
+					}
+					testNG.createSuite(suiteName, pTest);
+					testNG.addListener("listener.MyTestListener");
+					String suiteFilePath = System.getProperty("user.dir") + "/src/test/resources/projectJSONs/" + suiteFileName;
+					JSONParser suiteParser = new JSONParser();
+					JSONObject suiteClassJSON = (JSONObject) suiteParser.parse(new FileReader(new File(suiteFilePath)));
+					JSONArray testCases = (JSONArray) suiteClassJSON.get("testCases");
+					for (int k = 0; k < testCases.size(); k++) {
+						JSONObject testCase = (JSONObject) testCases.get(k);
+						String testName = (String) testCase.get("name");
+						testNG.addTest(testName);
+						JSONArray executions = (JSONArray) testCase.get("executions");
+						for (int l = 0; l < executions.size(); l++) {
+							JSONObject execution = (JSONObject) executions.get(l);
+							JSONArray parameterNames = (JSONArray) execution.get("parameternames");
+							JSONArray parameterValues = (JSONArray) execution.get("parametervalues");
+							if (parameterNames.size() != 0 && parameterValues.size() != 0) {
+								for (int m = 0; m < parameterNames.size(); m++) {
+									testNG.adddTestParameter(parameterNames.get(m).toString(), parameterValues.get(m).toString());
+								}
+							}
+							String dataFlag = (String) execution.get("dataflag");
+							JSONArray methods = (JSONArray) execution.get("methods");
+							List<String> includeMethods = new ArrayList<String>();
+							for (int n = 0; n < methods.size(); n++) {
+								String method = (String) methods.get(n);
+								String currentClassName = classMethodMap.get(method);
+								if (n == methods.size() - 1
+										|| !currentClassName.equals(classMethodMap.get(methods.get(n + 1)))) {
+									includeMethods.add(method);
+									testNG.addTestClass(currentClassName, includeMethods);
+									includeMethods = new ArrayList<String>();
+								} else {
+									includeMethods.add(method);
+								}
+
+							}
+						}
+					}
+				}
+			}
+			testNG.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
